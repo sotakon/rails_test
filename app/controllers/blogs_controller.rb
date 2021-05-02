ebdf594e40ad750bac8935a195acdce499e10e90
sotakon <skon1221@gmail.com>
2021-05-02 16:09:16 +0900

    create: create action追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,12 @@
 class BlogsController < ApplicationController
   def index
   end
+
+  def new
+    @blogs = Blog.new
+  end
+
+  def create
+    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+  end
 end
