34e15fd1d37e37c9e0e6bb72e17e3cb3d7b01599
sotakon <skon1221@gmail.com>
2021-05-02 22:50:41 +0900

    update: ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :blogs do
     collection do
       post :confirm
     end
   end
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
