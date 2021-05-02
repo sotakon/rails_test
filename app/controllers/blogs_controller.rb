7ff7ce8282e2ea971e43b6f3a0c24a2007b987c9
sotakon <skon1221@gmail.com>
2021-05-02 19:54:13 +0900

    create: edit action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,44 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update]
   def index
+    @blogs = Blog.all
   end
 
   def new
-    @blogs = Blog.new
+    @blog = Blog.new
   end
 
   def create
-    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
